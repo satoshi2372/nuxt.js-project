967020498b50c910bccaeb82a9810797d3ae5b64
satoshi2372 <ape2372@gmail.comgit config --global user.email ape2372@gmail.com>
2020-11-20 13:07:50 +0900

    非同期通信のエラーハンドリグ

--- hello/pages/index.vue
+++ hello/pages/index.vue
@@ -19,13 +19,20 @@ const axios = require('axios')
 let url = 'https://jsonplaceholder.typicode.com/users'
 export default {
   //asyncData nuxtメソッド コンポーネント初期化前に非同期の処理を行えるようにするためのメソッド
-  asyncData({ params }){
+  asyncData({ params,error }){
     //指定アドレスからのAPIデータ取得、取得後に前メソッド呼ぶ
     return axios.get(url)
     //resにはサーバーからのレスポンスデータが入っている
     .then((res)=> {
       //res.dataにはjsonデータが入っている。
       return {users: res.data}
+    })
+    //エラーハンドリング eはエラー情報 response.statusでエラー内容を表示できる
+    .cach((e) => {
+      console.log(e.response.status)
+      // エラーページの表示には下記をセットで記述 message:の後に文章を入力する事で好きな文章に指定できる。
+      error({users: e.response.status, message:e.message});
+      // error({users: e.response.status, message: 'Error!!!!' });
     })
   }
 }
